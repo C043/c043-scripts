14d254dbccf201eb7f15e6204099924174365cf5
C043 <mariofragnitoph@gmail.com>
2024-12-26 23:47:13 +0100

    Refactor masterChefCurl.sh

--- masterChefCurl.sh
+++ masterChefCurl.sh
@@ -4,6 +4,6 @@ COUNT=6
 while read -r url; do
     NAME="Episode $COUNT.mp4"
     echo "Downloading $NAME"
-    curl -o "Episode $COUNT.mp4" $url
+    curl -o $NAME $url
     COUNT=$(($COUNT + 1))
 done < "/home/c043/c043-scripts/urls.txt"
