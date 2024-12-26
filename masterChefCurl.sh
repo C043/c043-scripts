2325750b770c8d741f892cbd4d489e6b9dc61769
C043 <mariofragnitoph@gmail.com>
2024-12-26 23:15:27 +0100

    Optimize CurlQueue.sh

--- masterChefCurl.sh
+++ masterChefCurl.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
-COUNT=4
+COUNT=6
 while read -r url; do
+    NAME="Episode $COUNT.mp4"
+    echo "Downloading $NAME"
     curl -o "Episode $COUNT.mp4" $url
     COUNT=$(($COUNT + 1))
 done < "/home/c043/c043-scripts/urls.txt"
