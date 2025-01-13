0ce4f4835bd6147d97d78b513bb101d0c6abaff7
C043 <mariofragnitoph@gmail.com>
2025-01-13 13:12:55 +0100

    Fixed script

--- productivity.sh
+++ productivity.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-vivaldi.vivaldi-stable "https://www.youtube.com/watch?v=u4UZ4UvZXrg" &
+vivaldi.vivaldi-stable --new-window "https://www.youtube.com/watch?v=u4UZ4UvZXrg" &
 sleep 1
 vivaldi.vivaldi-stable "https://www.twitch.tv/studytme" &
