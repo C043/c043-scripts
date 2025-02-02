d98687ae986ff0feea8b0d7281bd5f8f86eef2a1
C043 <mariofragnitoph@gmail.com>
2025-02-02 23:21:40 +0100

    Adds window.sh

--- productivity.sh
+++ productivity.sh
@@ -1,8 +1,5 @@
 #!/bin/zsh
 
-# vivaldi.vivaldi-stable --new-window "https://www.youtube.com/watch?v=u4UZ4UvZXrg" &
-#sleep 1
-# vivaldi.vivaldi-stable "https://www.twitch.tv/studytme" &
 vivaldi.vivaldi-stable --new-window "https://webmail.register.it/appsuite/#!!&app=io.ox/mail&folder=default0/INBOX" &
 sleep 1
 vivaldi.vivaldi-stable "https://mail.google.com/chat/u/1/#chat/space/AAAAvvo6XTk" &
---
+++ window.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+ vivaldi.vivaldi-stable --new-window "https://www.youtube.com/watch?v=u4UZ4UvZXrg" &
+sleep 1
+ vivaldi.vivaldi-stable "https://www.twitch.tv/studytme" &
