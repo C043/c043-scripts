4cac93203282cf28f5bdd771f2bd27537935182c
C043 <mariofragnitoph@gmail.com>
2025-03-17 09:05:44 +0100

    Updates productivity script

--- productivity.sh
+++ productivity.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 vivaldi.vivaldi-stable --new-window "https://webmail.register.it/appsuite/#!!&app=io.ox/mail&folder=default0/INBOX" &
-sleep 1
+sleep 2
 vivaldi.vivaldi-stable "https://mail.google.com/chat/u/1/#chat/space/AAAAvvo6XTk" &
-sleep 1
+sleep 2
 vivaldi.vivaldi-stable "https://trello.com/b/fGWIkmKH/stocktitan" &
