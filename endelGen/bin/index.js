93555ea01e40f848b4527ab4b6302cf6b44ee3a6
C043 <mariofragnitoph@gmail.com>
2025-06-08 23:58:41 +0200

    Builded script endelGen

--- endelGen/bin/index.js
+++ endelGen/bin/index.js
@@ -3,7 +3,7 @@
 import puppeteer from "puppeteer";
 import { Mailsac } from "@mailsac/api";
 import dotenv from "dotenv";
-import { dirname, resolve } from "path";
+import { dirname, parse, resolve } from "path";
 import { fileURLToPath } from "url";
 
 const _filename = fileURLToPath(import.meta.url);
@@ -25,11 +25,37 @@ const readAddressInbox = async email => {
   return data;
 };
 
-const email = generateRandomAddress();
-console.log(`Using temporary email: ${email}`);
+const getMessageContent = async (email, messageId) => {
+  const { data } = await mailsac.messages.getFullRawMessage(email, messageId);
+  return data;
+};
 
 const wait = async ms => {
   return new Promise((resolve, reject) => setTimeout(resolve, ms));
+};
+
+const parseCodeFromEmail = text => {
+  const match = text.match(/\b\d{4}-\d{4}-\d{4}\b/);
+  const code = match ? match[0] : null;
+  return code;
+};
+
+const getNewCode = async (email, messagesCount) => {
+  let messagesReceived = messagesCount;
+  while (messagesReceived <= messagesCount) {
+    await wait(5000);
+    const messages = await readAddressInbox(email);
+    if (messages.length > messagesCount) {
+      messagesReceived++;
+      const fullMessage = await getMessageContent(
+        email,
+        messages[messages.length - 1]._id
+      );
+      const code = parseCodeFromEmail(fullMessage);
+      console.log(`New code: ${code}`);
+      return code;
+    }
+  }
 };
 
 const createAccount = async email => {
@@ -62,14 +88,32 @@ const createAccount = async email => {
     )
     .click();
 
-  console.log(`Messages:`);
-  while (messagesReceived <= 0) {
-    const messages = await readAddressInbox(email);
-    console.log(messages);
-  }
+  const code = await getNewCode(email, 0);
+  await page.type(
+    "#__next > div.Popup_wrap__xTO_Y.Popup_true__LQ6s8 > div > div.Wrap_wrap__lYIgN.Wrap_small___tL01.Auth_wrap__1MMvx > div > div.FormWrap_wrap__30Ofn.FormWrap_black__IzHjW > div > div > form > label > input",
+    code
+  );
+  await wait(1000);
+  await page
+    .locator(
+      "#__next > div.Popup_wrap__xTO_Y.Popup_true__LQ6s8 > div > div.Wrap_wrap__lYIgN.Wrap_small___tL01.Auth_wrap__1MMvx > div > div.FormWrap_wrap__30Ofn.FormWrap_black__IzHjW > div > div > form > button.Button_button__Ui8H1.Button_normal__stuQD.Button_border__wbP1B > div"
+    )
+    .click();
+  await wait(1000);
+  await page.type(
+    "#__next > div.Popup_wrap__xTO_Y.Popup_true__LQ6s8 > div > div.Wrap_wrap__lYIgN.Wrap_small___tL01.Auth_wrap__1MMvx > div > div.FormWrap_wrap__30Ofn.FormWrap_black__IzHjW > div > div > form > label > input",
+    "C043"
+  );
+  await page
+    .locator(
+      "#__next > div.Popup_wrap__xTO_Y.Popup_true__LQ6s8 > div > div.Wrap_wrap__lYIgN.Wrap_small___tL01.Auth_wrap__1MMvx > div > div.FormWrap_wrap__30Ofn.FormWrap_black__IzHjW > div > div > form > button > div"
+    )
+    .click();
 
   return browser;
 };
 
+const email = generateRandomAddress();
+console.log(`Using temporary email: ${email}`);
 await createAccount(email);
-console.log("Done");
+console.log("Done.");
