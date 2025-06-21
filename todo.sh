23fc5438c3a0e9d36236a7d70079a526c13cc18c
Mario Fragnito <mariofragnitoph@gmail.com>
2025-06-21 17:48:59 +0200

    Adds macOs compatibility to todo.sh script

--- todo.sh
+++ todo.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cd ~
-cd ./tpuie-ddjxa
-nvim ./To-Do.md
+OS=$(uname)
+
+if [ "$OS" == "Linux" ]; then
+	cd ~
+	cd ./tpuie-ddjxa
+	nvim ./To-Do.md
+elif [ "$OS" == "Darwin" ]; then
+	cd '/Users/ultramaggot/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian Vault/'
+	nvim ./To-Do.md
+fi
