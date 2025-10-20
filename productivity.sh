a7896067a7361ee91be8d6fab6ab6cbef060275a
C043 <mariofragnitoph@gmail.com>
2025-10-20 10:12:02 +0200

    Changed browser for productivity.sh in linux mode

--- productivity.sh
+++ productivity.sh
@@ -17,11 +17,11 @@ case "$(uname -s)" in
     ;;
   Linux*)
     # ---- Linux (original commands) ----
-    nohup vivaldi --new-window "${urls[1]}" > /dev/null 2>&1 &
+    nohup google-chrome --new-window "${urls[1]}" > /dev/null 2>&1 &
     sleep 2
-    nohup vivaldi "${urls[2]}" > /dev/null 2>&1 &
+    nohup google-chrome "${urls[2]}" > /dev/null 2>&1 &
     sleep 2
-    nohup vivaldi "${urls[3]}" > /dev/null 2>&1 &
+    nohup google-chrome "${urls[3]}" > /dev/null 2>&1 &
     ;;
   *)
     echo "Unsupported platform: $(uname -s)" >&2
