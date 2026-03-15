02f22c3d123ba69c30948b208f3ff3f3578da48e
C043 <mariofragnitoph@gmail.com>
2026-03-15 17:07:25 +0100

    Starts working on comicBookSnatcher bot

---
+++ comicBookSnatcher/main.py
@@ -0,0 +1,22 @@
+def fetchResults(url):
+    return None
+
+
+def parseResults(html, comicName):
+    return None
+
+
+def load_state():
+    return None
+
+
+def saveState(state):
+    return None
+
+
+def findNewComics(items, state, comicName):
+    return None
+
+
+def sendTelegramPhoto(token, chatId, item):
+    return None
