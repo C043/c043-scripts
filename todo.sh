3a4690efd38b049cc230b70b058630655fb3bbef
C043 <mariofragnitoph@gmail.com>
2026-04-04 23:40:26 +0200

    Reformatted file

--- todo.sh
+++ todo.sh
@@ -3,10 +3,10 @@
 OS=$(uname)
 
 if [ "$OS" == "Linux" ]; then
-	cd ~
-	cd ./Sync
-	nvim ./To-Do.md
+    cd ~
+    cd ./Sync
+    nvim ./To-Do.md
 elif [ "$OS" == "Darwin" ]; then
-	cd '/Users/ultramaggot/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian Vault/'
-	nvim ./To-Do.md
+    cd '/Users/ultramaggot/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian Vault/'
+    nvim ./To-Do.md
 fi
