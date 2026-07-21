22fddf75189c020af49813f0e6efab6787ee2d8b
Mario Fragnito <mariofragnitoph@gmail.com>
2026-07-21 15:34:04 +0200

    Changed vault.sh

--- vault.sh
+++ vault.sh
@@ -7,6 +7,6 @@ if [ "$OS" == "Linux" ]; then
     cd ./Sync/Obsidian
     nvim
 elif [ "$OS" == "Darwin" ]; then
-    cd '/Users/ultramaggot/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian Vault/'
+    cd '/Users/ultramaggot/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian Vault/Obsidian/'
     nvim
 fi
